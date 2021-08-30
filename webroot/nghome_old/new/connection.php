76ecedbd15c4ad8229d987e87778093a86220ea6
MauricioAlcala <a_mauricio@live.com>
2021-08-30 12:02:26 -0700

    gym

---
+++ webroot/nghome_old/new/connection.php
@@ -0,0 +1,11 @@
+
+<?php
+$servername = "localhost";
+$username = "u964887345_gym";
+$password = "98042663387Jd";
+$dbname="u964887345_gym";
+$server_path = 'http://gym.nacionalcode.online/';
+$image_path='http://gym.nacionalcode.online/webroot/upload/';
+$image_pa=$image_path;
+$conn = new mysqli($servername,$username, $password,$dbname);
+?>
